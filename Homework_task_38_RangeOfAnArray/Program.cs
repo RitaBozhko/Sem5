ba4aa12b8eaa32f17762a782d07ba1c4676e772f
RitaBozhko <79147042+RitaBozhko@users.noreply.github.com>
2022-08-04 15:59:15 +0300

    fixed Function

--- Homework_task_38_RangeOfAnArray/Program.cs
+++ Homework_task_38_RangeOfAnArray/Program.cs
@@ -19,13 +19,13 @@ void PrintArray(int n)
 
 int FindRange(int[] arr)
 {
-    int max = array[0];
-    int min = array[0];
+    int max = arr[0];
+    int min = arr[0];
 
     for (int i = 0; i < n; i++)
     {
-        if (array[i] > max) max = array[i];
-        else if (array[i] < min) min = array[i];
+        if (arr[i] > max) max = arr[i];
+        else if (arr[i] < min) min = arr[i];
     }
     return max-min;
 }
