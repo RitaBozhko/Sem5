a9dc08979e1017bf6d5b9bd69c73d2d2f3009db5
RitaBozhko <79147042+RitaBozhko@users.noreply.github.com>
2022-08-04 15:54:18 +0300

    Homework Seminar 5

---
+++ Homework_task_34_SumOfEvenNumsInAnArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 34: 
+// Задайте массив 
+// заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Какого размера вы хотите массив? ");
+bool userEnter = int.TryParse(Console.ReadLine(), out int n);
+int[] array = new int[n];
+
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    array[i] = new Random().Next(100,1000);
+    Console.Write($"{array[i]}, ");
+    if (array[i]%2 == 0) count ++;
+}
+
+Console.WriteLine($"\nКоличество четных чисел в данном массиве равно {count}");
---
+++ Homework_task_38_RangeOfAnArray/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 38: 
+// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+Console.WriteLine("Какого размера вы хотите массив? ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+
+void PrintArray(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(1,100);
+        Console.Write($"{array[i]}, ");         
+    }
+}
+
+int FindRange(int[] arr)
+{
+    int max = array[0];
+    int min = array[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] > max) max = array[i];
+        else if (array[i] < min) min = array[i];
+    }
+    return max-min;
+}
+
+PrintArray(n);
+Console.WriteLine($"\nРазница между максимальным и минимальным значениями в данном массиве равна {FindRange(array)}");
